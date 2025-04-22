886e463d47c4e37201fc08ea5eacb724236cdc0e
Tom Broucke <tom@tombroucke.be>
2025-04-22 10:09:00 +0200

    🩹 fix: load textdomain by removing translation

--- config/html-forms-captcha.php
+++ config/html-forms-captcha.php
@@ -11,19 +11,19 @@ return [
 
     'strings' => [
         'recaptcha' => [
-            'site_key_not_set' => __('<code>RECAPTCHA_SITE_KEY</code> is not set.', 'sage'),
-            'secret_key_not_set' => __('<code>RECAPTCHA_SECRET_KEY</code> is not set.', 'sage'),
-            'missing_configuration' => __('reCAPTCHA has been disabled due to missing configuration. See admin notices for more information.', 'sage'),
-            'invalid_captcha' => __('Invalid reCAPTCHA response. Did you check the "I am human" box?', 'sage'),
+            'site_key_not_set' => '<code>RECAPTCHA_SITE_KEY</code> is not set.',
+            'secret_key_not_set' => '<code>RECAPTCHA_SECRET_KEY</code> is not set.',
+            'missing_configuration' => 'reCAPTCHA has been disabled due to missing configuration. See admin notices for more information.',
+            'invalid_captcha' => 'Invalid reCAPTCHA response. Did you check the "I am human" box?',
         ],
         'hcaptcha' => [
-            'site_key_not_set' => __('<code>HCAPTCHA_SITE_KEY</code> is not set.', 'sage'),
-            'secret_key_not_set' => __('<code>HCAPTCHA_SECRET_KEY</code> is not set.', 'sage'),
-            'missing_configuration' => __('hCaptcha has been disabled due to missing configuration. See admin notices for more information.', 'sage'),
-            'invalid_captcha' => __('Invalid hCaptcha response. Did you check the "I am human" box?', 'sage'),
+            'site_key_not_set' => '<code>HCAPTCHA_SITE_KEY</code> is not set.',
+            'secret_key_not_set' => '<code>HCAPTCHA_SECRET_KEY</code> is not set.',
+            'missing_configuration' => 'hCaptcha has been disabled due to missing configuration. See admin notices for more information.',
+            'invalid_captcha' => 'Invalid hCaptcha response. Did you check the "I am human" box?',
         ],
-        'configuration_errors' => __('Sage HTML Forms Captcha configuration errors:', 'sage'),
-        'could_not_validate_captcha' => __('Error: Could not validate captcha. Please contact us through other means.', 'sage'),
+        'configuration_errors' => 'Sage HTML Forms Captcha configuration errors:',
+        'could_not_validate_captcha' => 'Error: Could not validate captcha. Please contact us through other means.',
     ],
 
 ];
